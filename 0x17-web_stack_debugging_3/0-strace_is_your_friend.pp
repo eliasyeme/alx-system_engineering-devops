4e693ada23b94527a7b71edc69ae8dd56bcc103b
eliasyeme <eliasyeme@gmail.com>
2024-02-13 15:27:34 +0300

    debug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# fix server error by replacing phpp with php
+
+
+exec {'replace-line':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
