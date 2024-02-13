066d7c149ab49e3e4cb68526a69b7730214e0aac
eliasyeme <eliasyeme@gmail.com>
2024-02-13 14:44:18 +0300

    Get subscriber number api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Query the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit,
+             or 0 if the subreddit doesn't exist or an error occurs.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get("data").get("subscribers")
