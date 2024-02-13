a4decd2b1375d34e75023c963e27448574ef7c30
eliasyeme <eliasyeme@gmail.com>
2024-02-13 15:06:13 +0300

    Recursive querie

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Recursive function that queries the Reddit API and returns a list
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Get the titles of the first 10 hot posts for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".format(
+        subreddit, after
+    )
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    for post in response.json().get("data").get("children"):
+        hot_list.append(post.get("data").get("title"))
+    after = response.json().get("data").get("after")
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
